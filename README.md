ede4fb8f24323281ab15dcc7c6e14e6bbf7ed675
wait-what <25844213+wait-what@users.noreply.github.com>
2023-05-01 17:56:15 +0300

    feat: add coverage.png generation

--- README.md
+++ README.md
@@ -11,13 +11,33 @@ This generates:
 - Yarn (npm if you're a masochist)
 
 ## Usage
-```
-mkdir data
-wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P data/
-cp ../out/test_json/mtnt_test.json data/
-```
+- Gather the required files
+    - Make the data directory
+    ```
+    mkdir data
+    ```
+    - Download unicode data
+    ```
+    wget https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json -P ./data/
+    ```
+    - Download Inter font
+    ```
+    wget https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip -P data/
+    unzip ./data/Inter-3.19.zip -d ./data/inter
+    mv ./data/inter/Inter\ Hinted\ for\ Windows/Desktop/Inter-SemiBold.ttf ./data/inter.ttf
+    rm -rf ./data/inter ./data/Inter-3.19.zip
+    ```
+    - Copy Mutant remix data
+    ```
+    cp ../out/test_json/mtnt_test.json ./data/
+    ```
 
+- Install dependencies
 ```
 yarn
+```
+
+- Run
+```
 yarn start
 ```
--- src/image.ts
+++ src/image.ts
@@ -1,17 +1,33 @@
 import { logger } from '.'
 import { readFileSync, writeFileSync, mkdirSync } from 'fs'
 import { CoverageData } from './types'
+import { Resvg } from '@resvg/resvg-js'
 
 export default async (data: CoverageData) => {
     const baseSvg = readFileSync('./src/base_coverage.svg', 'utf8')
+
+    mkdirSync('./out', { recursive: true })
 
     const svg = baseSvg
         .replace('${missing}', data.coverage.mutantMissing.toString())
         .replace('${satisfied}', data.coverage.mutantSatisfied.toString())
         .replace('${progress}', (data.coverage.satisfiedPercentage * 1.95).toString())
 
-    mkdirSync('./out', { recursive: true })
     writeFileSync('./out/coverage.svg', svg)
-
     logger.done('Wrote svg to ./out/coverage.svg')
+
+    const resvg = new Resvg(svg, {
+        fitTo: {
+            mode: 'width',
+            value: 2048,
+        },
+        font: {
+            fontFiles: [ './data/inter.ttf' ],
+            loadSystemFonts: false,
+        },
+    })
+    const png = resvg.render().asPng()
+
+    writeFileSync('./out/coverage.png', png)
+    logger.done('Wrote png to ./out/coverage.png')
 }
