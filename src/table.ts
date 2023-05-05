95751996f054cdbf4f8e5e9b60ba935c240fc30a
wait-what <25844213+wait-what@users.noreply.github.com>
2023-05-05 17:42:13 +0300

    feat: update coverage table layout

--- src/table.ts
+++ src/table.ts
@@ -10,19 +10,18 @@ export default (data: Map<string, Emoji[]>) => {
     for (const group of data) {
         const groupMd = [
             `## ${group[0]}\n`,
-            [ 'Codepoint', 'Shortcode', 'Emoji', 'Emoticons', 'Alternates', 'Mutant' ].join(' | '),
-            [ '---', '---', '---', '---', '---', '---' ].join(' | '),
+            [ 'Codepoint', 'Shortcode', 'Emoji', 'Mutant Remix' ].join(' | '),
+            [ '---', '---', '---', '---' ].join(' | '),
         ]
 
         for (const emoji of group[1]) {
             const src = emoji.mutant?.src.replace(']', '%5D').replace('[', '%5B').replace(/\s/g, '%20')
 
             groupMd.push([
-                `[\`${emoji.codepoint}\`](https://emojipedia.org/search/?q=${emoji.codepoint})`,
+                `[\`${emoji.codepoint}\`](https://emojipedia.org/search/?q=${emoji.codepoint})` +
+                (emoji.alternates.length > 0 ? ` + ${emoji.alternates.length}` : ''),
                 emoji.shortcodes.map(shortcode => `\`${shortcode}\``).join(', '),
                 emoji.codepoint.split('-').map(chunk => String.fromCodePoint(parseInt(chunk, 16))).join(''),
-                emoji.emoticons.map(emoticon => `\`${emoticon}\``).join(', ') || '',
-                emoji.alternates.length || '',
                 emoji.mutant ? `[${emoji.mutant.shortcode}](https://github.com/mutant-remix/assets/tree/master/svg/${src})` : '❌',
             ].join(' | '))
         }
